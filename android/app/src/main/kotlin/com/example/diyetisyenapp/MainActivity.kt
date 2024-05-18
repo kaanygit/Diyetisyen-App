d2ac670206c007e8d5b2b7254a6eff435bc5b55f
kaanygit <yasinkaan345@hotmail.com>
2024-05-18 17:40:39 +0300

    added login page

---
+++ android/app/src/main/kotlin/com/example/diyetisyenapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.diyetisyenapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:diyetisyenapp/firebase_options.dart';
+import 'package:diyetisyenapp/pages/home_page.dart';
+import 'package:diyetisyenapp/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GlobalLoaderOverlay(
+      overlayColor: Colors.grey.withOpacity(0.4),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          primarySwatch: Colors.indigo,
+        ),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) =>
+              snapshot.data != null ? HomePage() : LoginPage(),
+        ),
+      ),
+    );
+  }
+}
